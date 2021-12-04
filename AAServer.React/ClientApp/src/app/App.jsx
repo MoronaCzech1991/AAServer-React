d99b77ba2b1d896856d069483189c2428b66a6cb
Daks Marlon Tancredo <marlon_tancredo@hotmail.com>
2021-12-04 16:59:37 +0000

    Marlon - organizacao de pastas

--- AAServer.React/ClientApp/src/app/App.jsx
+++ AAServer.React/ClientApp/src/app/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import SignIn from './components/telaCadastro/signin';
+import SignIn from '../components/signin';
 
 const App = () => {
   return (
--- AAServer.React/ClientApp/src/index.jsx
+++ AAServer.React/ClientApp/src/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SignIn from './components/telaCadastro/signin';
+import SignIn from './components/signin';
 
 ReactDOM.render(<SignIn />, document.getElementById('root'));
