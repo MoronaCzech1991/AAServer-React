9d7804ca1a942a4f4f3e358c8006519326de967f
Daks Marlon Tancredo <marlon_tancredo@hotmail.com>
2021-12-04 13:44:31 +0000

    teste

---
+++ AAServer.React/ClientApp/src/components/teste/Index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import * as S from './styles';
+
+const Teste = () => {
+  return (
+    <S.Container>
+      <h1>Teste do porco</h1>
+    </S.Container>
+  );
+};
+
+export default Teste;
